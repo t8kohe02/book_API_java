275b504d733f9f5e7e57c331a2d8c7cedf6e5056
t8kohe02 <t8kohe02@students.oamk.fi>
2024-09-19 17:47:03 +0300

    empty frame

---
+++ src/test/java/fi/korpi/bookapi/BookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.korpi.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
