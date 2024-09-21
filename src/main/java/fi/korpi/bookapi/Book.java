f04fa9b7857dd4eec0b0df08f6d91a5f742a59be
t8kohe02 <t8kohe02@students.oamk.fi>
2024-09-21 20:57:03 +0300

    MVP build success

--- src/main/java/fi/korpi/bookapi/Book.java
+++ src/main/java/fi/korpi/bookapi/Book.java
@@ -10,11 +10,44 @@ public class Book {
     private Long id;
 
     private String title;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
     private String author;
     private int publicationYear;
     private String publisher;
     private String description;
-
-    // Getters and setters
-    // Constructors
 }
